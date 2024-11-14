d54f0c217213e380b3032743642b10b5c819f43b
vselenaya <71250580+vselenaya@users.noreply.github.com>
2024-11-14 22:15:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const url = 'https://cats.is-course.ru/settings';
     fetch(url, {
       method: 'POST',
       credentials: 'include',
-      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
